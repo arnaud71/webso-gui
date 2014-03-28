5fbaf6ee6367db522129aa77c9a6a07a4c58c8be
salahZen <salah_zenati@live.fr>
2014-03-28 13:33:58 +0100

    update

--- test/spec/e2e/controllers/controllersSpec.js
+++ test/spec/e2e/controllers/controllersSpec.js
@@ -6,28 +6,21 @@ describe("Integration/E2E Testing", function() {
   it('pouvoir aller à la page de log in', function() {
     browser.get('http://localhost:9000/#/signin');
     
-    /*
-    var inputPseudo = element(by.name('pseudo'));
+    var inputPseudo = element(by.name('email'));
     inputPseudo.clear();
     inputPseudo.sendKeys('test_pseudo');
-    */
     
-    /*
+    
     var inputMotDePasse = element(by.name('password'));
     inputMotDePasse.clear();
     inputMotDePasse.sendKeys('mot_de_passe');
-    */
-
-    /*
-    var checkBoxSeRappeler = element(by.name('se_rappeler'));
+    
+    var checkBoxSeRappeler = element(by.name('remember'));
     checkBoxSeRappeler.click();
-    */
 
-    /*
     var button = element(by.name('Connexion'));
-    button.click();
-    */
- 	});
+    button.click();    
+ });
 /*********************************************************************************************************/
   it('pouvoir aller à la page Valider > Ajouter information et ajouter une information valide', function() {
     browser.get('http://localhost:9000/#/validate/add');
@@ -48,8 +41,8 @@ describe("Integration/E2E Testing", function() {
     inputDetails.clear();
     inputDetails.sendKeys('test detail');
 
-    // var button = element(by.button('doAdd()'));
-    // button.click();
+    var button = element(by.name('validate'));
+    button.click();
   });
 /*********************************************************************************************************/
   it('pouvoir aller à la page Valider > Afficher informations validées', function() {
@@ -61,11 +54,10 @@ describe("Integration/E2E Testing", function() {
     browser.get('http://localhost:9000/#/watch/add');
 
 
-    /*
-    var inputSource = element(by.input('text'));
+    
+    var inputSource = element(by.name('inputUrl'));
     inputSource.clear();
     inputSource.sendKeys('test source');
-    */
     
     var inputTitle = element(by.input('inputTitle'));
     inputTitle.clear();
@@ -79,22 +71,18 @@ describe("Integration/E2E Testing", function() {
     // choisir le domaine de surveillance. c'est un menu de choix
     // choisir le secteur d'activité pour la surveillance. c'est un menu de choix
 
-    /*
-    var inputRequete = element(by.input('inputRequete'));
+    var inputRequete = element(by.name('requete'));
     inputRequete.clear();
     inputTitle.sendKeys('test requête');
-    */
 
-    /*
-     var button = element(by.button('btn'));
+     var button = element(by.name('testButton'));
      button.click();
-    */
     
     // choisir une frequence pour la surveillance. c'est un menu de choix
     // choisir le mode de notification pour la surveillance. c'est un menu de choix
 
-    // var button = element(by.button('doAdd()'));
-    // button.click();
+    var button = element(by.name('surveiller'));
+    button.click();
   });
 /*********************************************************************************************************/
   it('pouvoir aller à la page Surveiller > Ajouter une source et ajouter une source valide', function() {
@@ -112,10 +100,9 @@ describe("Integration/E2E Testing", function() {
     inputTags.clear();
     inputTags.sendKeys('test tag');
     
-    /*
-    var button = element(by.button('doAdd()'));
+    var button = element(by.name('add'));
     button.click();
-    */
+    
   });
 /*********************************************************************************************************/
   
@@ -136,26 +123,33 @@ describe("Integration/E2E Testing", function() {
 /*********************************************************************************************************/
   it('pouvoir aller à la page Diffuser > Newsletter et diffuser une newsletter valide', function() {
     browser.get('http://localhost:9000/#/publish/newsletter');
-    /*
-    var inputTitle = element(by.input('inputTitle'));
-    inputTitle.clear();
-    inputTitle.sendKeys('test lien');
-    */
 
-    /*
-    var inputEmail = element(by.input('inputEmail'));
+    var inputTitle = element(by.name('titleNL'));
+    inputTitle.clear();
+    inputTitle.sendKeys('test titre');
+
+    var inputEmail = element(by.name('email'));
     inputEmail.clear();
     inputEmail.sendKeys('test titre');
-    */
+
+    var buttonAddEmail = element(by.name('addItem'));
+    buttonAddEmail.click(); 
+    
+    var buttonOpenList = element(by.name('openList'));
+    buttonOpenList.click(); 
     
     // choisir la date d'envoie c'est en format spécial
+    var inputEmail = element(by.name('date'));
+    inputEmail.clear();
+    inputEmail.sendKeys('30-06-2014');
+
     // choisir une frequence d'envoie. c'est un menu de choix
-    // choisir le dossier de validation. c'est un menu de choix
+    // choisir le dossier de validation. c'est un menu de choix  
     
-    /*
-    var button = element(by.button('...'));
-    button.click(); 
-    */
+	/*    
+    var buttonCreateNl = element(by.buttonText('Créer la newsletter'));
+    buttonCreateNl.click();
+	*/	
   });
 /*********************************************************************************************************/
   it('pouvoir aller à la page Diffuser > Rapport et diffuser un rapport valide', function() {
@@ -165,13 +159,18 @@ describe("Integration/E2E Testing", function() {
     inputTitle.clear();
     inputTitle.sendKeys('test titre');
 
-    // choisir le format du rapport. c'est un menu de choix 
-    // selection de dossier de validation. c'est un menu de choix 
+    // choisir le format du rapport. c'est un menu de choix
 
+    // selection de dossier de validation. c'est un menu de choix 
+    
     /*
-    var button = element(by.button('...'));
+    var buttonSelectionFolder = element(by.name('openFolders'));
+    SelectionFolder.click();
+	*/
+
+    var button = element(by.name('createReport'));
     button.click(); 
-    */
+
   });
 /*********************************************************************************************************/
 
@@ -212,34 +211,29 @@ describe("Integration/E2E Testing", function() {
 /*********************************************************************************************************/  
   it('pouvoir aller à la page Organiser > Profils et ajouter un profil valide', function() {
     browser.get('http://localhost:9000/#/organize/profile');
-    /*
-    var inputNom = element(by.input('inputNom'));
+    
+    var inputNom = element(by.name('nom'));
     inputNom.clear();
     inputNom.sendKeys('test nom');
-    */
-
-    /*
-    var inputPrenom = element(by.input('inputPrenom'));
+  
+    var inputPrenom = element(by.name('prenom'));
     inputPrenom.clear();
     inputPrenom.sendKeys('test prenom');
-    */
 
-    /*
-    var inputEmail = element(by.input('inputEmail'));
+    var inputPrenom = element(by.name('role'));
+    inputPrenom.clear();
+    inputPrenom.sendKeys('test role');
+
+    var inputEmail = element(by.name('email'));
     inputEmail.clear();
     inputEmail.sendKeys('test email');
-    */
 
-    /*    
-    var inputPassword = element(by.textarea('inputPassword'));
+    var inputPassword = element(by.name('motDePasse'));
     inputPassword.clear();
     inputPassword.sendKeys('test password');
-    */
 
-    /*
-    var button = element(by.button('...'));
+    var button = element(by.name('sauvegarder'));
     button.click(); 
-    */
   });
 /*********************************************************************************************************/
 
@@ -259,14 +253,12 @@ describe("Integration/E2E Testing", function() {
   it('pouvoir aller à la page Rechercher > Collectes webso et lancer une recherche valide', function() {
     browser.get('http://localhost:9000/#/search/webso');
 
-    var searchTerm = element(by.input('searchTerm'));
+    var searchTerm = element(by.model('searchTerm'));
     searchTerm.clear();
     searchTerm.sendKeys('test search');
 
-    /*
-    var button = element(by.button('doAdd()'));
+    var button = element(by.name('searchButton'));
     button.click();
-    */
   });
 /*********************************************************************************************************/
 
@@ -279,14 +271,12 @@ describe("Integration/E2E Testing", function() {
   it('pouvoir aller à la page Rechercher > Flux RSS et lancer une recherche valide', function() {
     browser.get('http://localhost:9000/#/search/rss');
 
-    var searchTerm = element(by.input('searchTerm'));
+    var searchTerm = element(by.name('searchTerm'));
     searchTerm.clear();
     searchTerm.sendKeys('test search');
 
-    /*
-    var button = element(by.button('doAdd()'));
+    var button = element(by.buttonText('Search'));
     button.click();
-    */
   });
 /*********************************************************************************************************/
 });
