11052b1d1df9b874686e4c5bab6082fd995d7c69
salahZen <salah_zenati@live.fr>
2014-04-11 10:15:40 +0200

    add require(grunt-karma) to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
-  grunt.loadNpmTasks('grunt-karma');
+  require('grunt-karma')(grunt);
 
   // Define the configuration for all the tasks
   grunt.initConfig({
