60b1e7cb3aa00da44693c7beb5400a2f8b828e00
salahZen <salah_zenati@live.fr>
2014-05-05 09:21:12 +0200

    userRegisterCtrl.js added

---
+++ app/scripts/controllers/userRegisterCtrl.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('websoApp')
+    .controller('userRegisterCtrl', function ($scope, $resource, cfg) {
+
+    $scope.isSuccess = false;
+
+    $scope.informationAdd = $resource(cfg.urlServices+':action',
+        {action:'put.pl', type_s:'enregistrement', jeton_s : false, compteur_sessions_s : 0, callback:"JSON_CALLBACK"},
+        {get:{method:'JSONP'}});
+
+    $scope.register = function() {
+        $scope.informationAddResult = $scope.informationAdd.get({
+            user_s  : $scope.username,
+            password_s : $scope.password,
+            role_s : 'lecteur'
+        });
+
+        $scope.isSuccess = true;
+		$scope.message = "Le compte a été enregistré avec succès";
+        $scope.username = "";
+        $scope.password = "";
+    };
+});
