5403c9eb6c4d713e246763c024c14ae7e1d31f49
salahZen <salah_zenati@live.fr>
2014-06-10 11:04:49 +0200

    Connexion automatique après l'enregistrement public et affichage de messages d'erreurs dans le cas non passant

--- app/scripts/controllers/publicRegisterCtrl.js
+++ app/scripts/controllers/publicRegisterCtrl.js
@@ -1,43 +1,46 @@
 'use strict';
 
 angular.module('websoApp')
-    .controller('publicRegisterCtrl', function ($scope, $cookieStore, $location, $resource, cfg) {
+    .controller('publicRegisterCtrl', function ($scope, $resource, $cookieStore, $location, cfg) {
 
     $scope.isSuccess = false;
 
     $scope.informationAdd = $resource(cfg.urlServices+'db/:action',
-        {action:'put.pl', type_s:'user', jeton_s : false,
-        //compteur_sessions_s : 0
-        callback:"JSON_CALLBACK"},
+        {action:'register.pl', callback:"JSON_CALLBACK"},
         {get:{method:'JSONP'}});
 
-  	$scope.verifyLogin = $resource(cfg.urlServices+'db/:action',
-    	{action:'login.pl', callback:"JSON_CALLBACK"},
-      	{get:{method:'JSONP'}});
+    $scope.verifyLogin = $resource(cfg.urlServices+'db/:action',
+        {action:'login.pl', callback:"JSON_CALLBACK"},
+        {get:{method:'JSONP'}});
+
+    var login = function (username, password) {
+        // envoi d'informations de login au service pour valider l'authentification  
+        $scope.verifyLogin.get({user_s : username, password_s : password}).$promise.then(function(user) {
+            $cookieStore.put('Authenticated', true);
+            $cookieStore.put('username', $scope.username);
+            $cookieStore.put('password', $scope.password);
+            $cookieStore.put('userRole', user.role);
+            $location.path('/home');
+        });
+    };
 
     $scope.register = function() {
-	    // envoi d'informations de login au service pour valider l'authentification  
-		$scope.verifyLogin.get({user_s : $scope.username, password_s : $scope.password}).$promise.then(function(user) {
-			if(user.error){
-		        $scope.informationAdd.get({
-		            user_s  : $scope.username,
-		            password_s : $scope.password,
-		            role_s : 'veilleur'
-		        });
-		        $scope.isSuccess = true;
-		        $scope.register();
-				$scope.message = "Connexion ...";
-			}
-			else {
-				if(user.success){
-					$cookieStore.put('Authenticated', true);
-					$scope.isAuthenticated = true;
-					$cookieStore.put('username', $scope.username);
-					$cookieStore.put('password', $scope.password);
-					$cookieStore.put('userRole', user.role);
-					$location.path('/home');				
-				}
-			}
-		});
-  	};
+        $scope.informationAdd.get({user_s  : $scope.username, password_s : $scope.password, role_s : 'veilleur'}).$promise.then(function(user) {
+            if(user.error){
+                $scope.isError = true;
+                $scope.errorMessage = user.error;
+            }
+            else {
+                if(user.success){
+                    $scope.isSuccess = true;
+                    $scope.message = "Le compte a été enregistré avec succès -> Connexion ...";
+                    login($scope.username, $scope.password);
+                }
+            }
+        });
+    };
+
+    $scope.releaseError = function() {
+        $scope.isError = false;
+    }
 });
