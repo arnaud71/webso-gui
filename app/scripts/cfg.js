cf05efe51ddf6344aa4d750abe1a7f716e39a84d
gaudinat <arnaud.gaudinat@hesge.ch>
2014-07-07 14:50:05 +0200

    add 0 document message in check query watch

--- app/scripts/cfg.js
+++ app/scripts/cfg.js
@@ -1,13 +1,15 @@
 angular.module('websoApp').constant('cfg', {
 
-  versionWebso    : '0.5',
+  versionWebso    : '0.6',
+  errorConnect    : 'Problème de connexion avec le server, veuillez tester votre connexion à internet (ou le server n\'est pas disponible)',
+
+
   urlDB           : 'http://localhost:8983/',
   urlServices     : 'http://localhost/cgi-bin/webso-services/',
   urlBookmarklet  : '',
-  errorConnect    : 'Problème de connexion avec le server, veuillez tester votre connexion à internet (ou le server n\'est pas disponible)',
 
-  //urlDB : 'http://albator.hesge.ch:8983/',
-  //urlServices : 'http://albator.hesge.ch/cgi-bin/webso-services/',
-  //urlBookmarklet : 'http://albator.hesge.ch/web/webso-gui/'
+  //urlDB           : 'http://albator.hesge.ch:8983/',
+  //urlServices     : 'http://albator.hesge.ch/cgi-bin/webso-services/',
+  //urlBookmarklet  : 'http://albator.hesge.ch/web/webso-gui/'
 
-}) ;
+});
