e3a640e16895708eec325a779c5058a290ded1c2
bkandouz <Badr@C-TIPS-777B-2.local>
2014-09-01 01:20:20 +0200

    Implementation of analytics v1.0
    Please see the master thesis report for a detailed view of the
    implementation as well as the event tracking plan.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,12 +3,8 @@
 var websoApp = angular.module('websoApp', ['sample.widgets.affichageSource', 'sample.widgets.affichageSurveillance', 
     'sample.widgets.affichageDossiersValidation','sample.widgets.affichageDossiersSurveillance',
     'sample.widgets.affichageCollectesMultisources','sample.widgets.affichageFluxTwitter',
-    'sample.widgets.defaultWidget', 'adf', 'checklist-model', 'ngCookies', 
-<<<<<<< HEAD
-    'ngRoute','ui.bootstrap','ngResource','ngSanitize','ngGrid','ui.bootstrap.pagination', 'ui.router', 'LocalStorageModule', 'ui.tree', 'xeditable','angulartics', 'angulartics.google.analytics', 'angulartics.piwik']);
-=======
-    'ngRoute','ui.bootstrap','ngResource','ngSanitize','ngGrid','ui.bootstrap.pagination', 'ui.router', 'LocalStorageModule', 'ui.tree', 'xeditable','ui.select']);
->>>>>>> master
+    'sample.widgets.defaultWidget', 'adf', 'checklist-model', 'ngCookies',
+    'ngRoute','ui.bootstrap','ngResource','ngSanitize','ngGrid','ui.bootstrap.pagination', 'ui.router', 'LocalStorageModule', 'ui.tree', 'xeditable','ui.select','angulartics', 'angulartics.google.analytics', 'angulartics.piwik']);
 
 websoApp.config(function($stateProvider, $urlRouterProvider) {
     
