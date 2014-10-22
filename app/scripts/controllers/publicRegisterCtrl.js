1f6521e0de8d33e4b56b68d3e19025323d6c5741
Clément MILLET <clement.millet@hotmail.fr>
2014-10-22 08:56:38 +0200

    Add tree structure when register

--- app/scripts/controllers/publicRegisterCtrl.js
+++ app/scripts/controllers/publicRegisterCtrl.js
@@ -1,17 +1,48 @@
 'use strict';
 
 angular.module('websoApp')
-    .controller('publicRegisterCtrl', function ($scope, $resource, $cookieStore, $location, cfg) {
+    .controller('publicRegisterCtrl', function ($scope, $resource, $cookieStore, $location, cfg, $filter) {
 
     $scope.isSuccess = false;
 
     $scope.informationAdd = $resource(cfg.urlServices+'db/:action',
-        {action:'register.pl', callback:"JSON_CALLBACK"},
+        {action:'register.pl', callback:'JSON_CALLBACK'},
         {get:{method:'JSONP'}});
 
     $scope.verifyLogin = $resource(cfg.urlServices+'db/:action',
-        {action:'login.pl', callback:"JSON_CALLBACK"},
+        {action:'login.pl', callback:'JSON_CALLBACK'},
         {get:{method:'JSONP'}});
+
+    $scope.addResource = $resource(cfg.urlServices+'db/:action',
+        {action:'put.pl', callback:'JSON_CALLBACK'},
+        {get:{method:'JSONP'}});
+
+    var folder = {
+      edit : false,
+      data : [{
+        'id': 1,
+        'title': 'Racine',
+        'nodes': [
+          {
+            'id': 11,
+            'title': 'dossier.1',
+            'nodes': [
+              {
+                'id': 111,
+                'title': 'dossier.1.1',
+                'nodes': []
+              }
+            ]
+          },
+          {
+            'id': 12,
+            'title': 'dossier.2',
+            'nodes': []
+          }
+        ],
+      }]
+    };
+
 
     var login = function (username, password) {
         // sending login information to the service to validate the authentication
@@ -32,9 +63,21 @@ angular.module('websoApp')
                 $scope.errorMessage = user.error;
             }
             else {
+                $scope.sourceSaveTree = $scope.addResource.get({
+                    user_s          : $scope.username,
+                    type_s          : 'tree',
+                    title_t         : 'vfolder',
+                    content_s       : $filter('json')(folder.data)
+                });
+                $scope.sourceSaveTree = $scope.addResource.get({
+                    user_s          : $scope.username,
+                    type_s          : 'tree',
+                    title_t         : 'wfolder',
+                    content_s       : $filter('json')(folder.data)
+                });
                 if(user.success){
                     $scope.isSuccess = true;
-                    $scope.message = "Le compte a été enregistré avec succès -> Connexion ...";
+                    $scope.message = 'Le compte a été enregistré avec succès -> Connexion ...';
                     login($scope.username, $scope.password);
                 }
             }
