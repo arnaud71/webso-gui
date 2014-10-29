6392908cc845724f7655ef002ebcde670e2067ed
Clément MILLET <clement.millet@hotmail.fr>
2014-10-29 18:09:25 +0100

    First pages to be extracted

---
+++ app/scripts/i18n/resources-locale_fr.js
@@ -0,0 +1,65 @@
+[
+    {
+        "key": "_PASSWORD_",
+        "value": "Mot de passe"
+    },
+    {
+        "key": "_CONFIRM_PASSWORD_",
+        "value": "Confirmer le mot de passe"
+    },
+    {
+        "key": "_EDIT_",
+        "value": "Modifier"
+    },
+    {
+        "key": "_SUCCESS_",
+        "value": "Succès"
+    },
+    {
+        "key": "_ERROR_",
+        "value": "Erreur"
+    },
+    {
+        "key": "_OK_",
+        "value": "Ok"
+    },
+    {
+        "key": "_CANCEL_",
+        "value": "Anuler"
+    },
+    {
+        "key": "_EMAIL_",
+        "value": "Adresse email"
+    },
+
+    {
+        "key": "_SERVICE_NAME_",
+        "value": "Webso"
+    },
+    {
+        "key": "_SETTINGS_BAR_FOLDER_",
+        "value": "Organiser"
+    },
+    {
+        "key": "_SETTINGS_BAR_ITEM_",
+        "value": "Gérer mes informations"
+    },
+    {
+        "key":"_SETTINGS_H1_",
+        "value":"Mon profil",
+        "description":"entête de div"
+    },
+
+    {
+        "key": "_USERLIST_H1_",
+        "value": "Compte des utilisateurs enregistrés"
+    },
+    {
+        "key": "_DELETE_USER_",
+        "value": "Effacement de l'utilisateur "
+    },
+    {
+        "key": "_ARE_YOU_SURE_",
+        "value": "Etes vous sûr de vouloir effacer cet utilisateur "
+    }
+]
