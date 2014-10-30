f19e33f5fb06305a19837500f91dd2ad52e5013b
Clément MILLET <clement.millet@hotmail.fr>
2014-10-30 09:08:05 +0100

    Add vfolder and edit profile

--- app/scripts/i18n/resources-locale_fr.js
+++ app/scripts/i18n/resources-locale_fr.js
@@ -31,46 +31,6 @@
         "key": "_EMAIL_",
         "value": "Adresse email"
     },
-
-    {
-        "key": "_SERVICE_NAME_",
-        "value": "Webso"
-    },
-    {
-        "key": "_SETTINGS_BAR_FOLDER_",
-        "value": "Organiser"
-    },
-    {
-        "key": "_SETTINGS_BAR_ITEM_",
-        "value": "Gérer mes informations"
-    },
-    {
-        "key":"_SETTINGS_H1_",
-        "value":"Mon profil",
-        "description":"entête de div"
-    },
-
-    {
-        "key": "_USERLIST_H1_",
-        "value": "Compte des utilisateurs enregistrés"
-    },
-    {
-        "key": "_DELETE_USER_",
-        "value": "Effacement de l'utilisateur "
-    },
-    {
-        "key": "_ARE_YOU_SURE_",
-        "value": "Etes vous sûr de vouloir effacer cet utilisateur "
-    },
-
-    {
-        "key": "_VALIDATE_",
-        "value": "Valider"
-    },
-    {
-        "key": "_ADD_INFORMATION_",
-        "value": "Ajouter une information"
-    },
     {
         "key": "_TITLE_",
         "value": "Titre"
@@ -95,6 +55,99 @@
         "key": "_FOLDER_",
         "value": "Dossier"
     },
+    {
+        "key": "_VALIDATE_",
+        "value": "Valider"
+    },
+    {
+        "key": "_SAVE_",
+        "value": "Sauvegarder"
+    },
+    {
+        "key": "_SEARCH_",
+        "value": "Rechercher"
+    },
+    {
+        "key": "_COLLAPSE_",
+        "value": "Replier"
+    },
+    {
+        "key": "_COLLAPSE_ALL_",
+        "value": "Replier tout"
+    },
+    {
+        "key": "_EXPAND_",
+        "value": "Déplier"
+    },
+    {
+        "key": "_EXPAND_ALL_",
+        "value": "Déplier tout"
+    },
+
+
+
+    {
+        "key": "_SERVICE_NAME_",
+        "value": "Webso",
+        "description": "Nom de la plateforme"
+    },
+
+
+
+    {
+        "key": "_ORGANIZE_BAR_ITEM_",
+        "value": "Organiser"
+    },
+    {
+        "key": "_ORGANIZE_BAR_PROFILE_",
+        "value": "Gérer mes informations"
+    },
+    {
+        "key": "_ORGANIZE_BAR_VFOLDER_",
+        "value": "Dossiers de validation"
+    },
+    {
+        "key": "_ORGANIZE_BAR_WFOLDER_",
+        "value": "Dossiers de surveillance"
+    },
+
+    {
+        "key":"_SETTINGS_H1_",
+        "value":"Mon profil",
+        "description":"entête de div"
+    },
+
+
+    {
+        "key": "_VFOLDER_H2_",
+        "value": "Organiser les dossiers de validation"
+    },
+    {
+        "key": "_VFOLDER_INFORMATION_",
+        "value": "Contenu du dossier"
+    },
+    {
+        "key": "_TREE_STRUCTURE_",
+        "value": "Arborescence"
+    },
+
+
+
+    {
+        "key": "_USERLIST_H1_",
+        "value": "Compte des utilisateurs enregistrés"
+    },
+    {
+        "key": "_DELETE_USER_",
+        "value": "Effacement de l'utilisateur "
+    },
+    {
+        "key": "_ARE_YOU_SURE_",
+        "value": "Etes vous sûr de vouloir effacer cet utilisateur "
+    },
+
+
+
     {
         "key": "_ADD_INFORMATION_",
         "value": "Ajouter une information"
