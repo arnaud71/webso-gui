5dcccb54190bed20efc935eb41ff2b6d69c333fa
Clément MILLET <clement.millet@hotmail.fr>
2015-01-08 17:01:50 +0100

    Test upload

--- app/scripts/controllers/AddInformationCtrl.js
+++ app/scripts/controllers/AddInformationCtrl.js
@@ -104,39 +104,74 @@ angular.module('websoApp')
         };
         $scope.doSearchFolder();
 
-        $scope.onFileSelect = function($files) {
-          //$files: an array of files selected, each file has name, size, and type.
-          for (var i = 0; i < $files.length; i++) {
-            var file = $files[i];
-            $scope.upload = $upload.upload({
-              //url: cfg.urlServices+'file/upload-test1.py',
-              url: cfg.urlServices+'file/file_upload.pl', //upload.php script, node.js route, or servlet url
-              //method: 'POST' or 'PUT',
-              method: 'POST',
-              headers: {'Access-Control-Request-Method': 'PUT, POST, OPTIONS'},
-              //withCredentials: true,
-              data: {myObj: $scope.myModelObj},
-              file: file, // or list of files ($files) for html5 only
-              //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-              // customize file formData name ('Content-Disposition'), server side file variable name. 
-              //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file' 
-              // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
-              //formDataAppender: function(formData, key, val){}
-            }).progress(function(evt) {
-              console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-            }).success(function(data, status, headers, config) {
-              // file is uploaded successfully
-              console.log(data);
-            });
-            //.error(...)
-            //.then(success, error, progress); 
-            // access or attach event listeners to the underlying XMLHttpRequest.
-            //.xhr(function(xhr){xhr.upload.addEventListener(...)})
-          }
-          /* alternative way of uploading, send the file binary with the file's content-type.
-             Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed. 
-             It could also be used to monitor the progress of a normal http post/put request with large data*/
-          // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
-        };
+        // $scope.onFileSelect = function($files) {
+        //   //$files: an array of files selected, each file has name, size, and type.
+        //   for (var i = 0; i < $files.length; i++) {
+        //     var file = $files[i];
+        //     $scope.upload = $upload.upload({
+        //       //url: cfg.urlServices+'file/upload-test1.py',
+        //       url: cfg.urlServices+'file/file_upload.pl', //upload.php script, node.js route, or servlet url
+        //       //method: 'POST' or 'PUT',
+        //       method: 'POST',
+        //       headers: {'Access-Control-Request-Method': 'PUT, POST, OPTIONS'},
+        //       //withCredentials: true,
+        //       data: {myObj: $scope.myModelObj},
+        //       file: file, // or list of files ($files) for html5 only
+        //       //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
+        //       // customize file formData name ('Content-Disposition'), server side file variable name. 
+        //       //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file' 
+        //       // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
+        //       //formDataAppender: function(formData, key, val){}
+        //     }).progress(function(evt) {
+        //       console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+        //     }).success(function(data, status, headers, config) {
+        //       // file is uploaded successfully
+        //       console.log(data);
+        //     });
+        //     //.error(...)
+        //     //.then(success, error, progress); 
+        //     // access or attach event listeners to the underlying XMLHttpRequest.
+        //     //.xhr(function(xhr){xhr.upload.addEventListener(...)})
+        //   }
+        //   /* alternative way of uploading, send the file binary with the file's content-type.
+        //      Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed. 
+        //      It could also be used to monitor the progress of a normal http post/put request with large data*/
+        //   // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
+        // };
+$scope.onFileSelect = function(file) {
+        var data = $scope.formUpload ? {
+          username: $scope.username
+        } : {};
 
+        file.upload = $upload.upload({
+          // url: 'https://angular-file-upload-cors-srv.appspot.com/upload' + $scope.getReqParams(),
+          url: cfg.urlServices+'file/file_upload_2.pl',
+          method: 'POST',
+          headers: {
+            'Access-Control-Request-Method': 'PUT, POST, OPTIONS'
+          },
+          data: data,
+          file: file,
+          fileFormDataName: 'myFile',
+        });
+
+        file.upload.then(function(response) {
+          // $timeout(function() {
+            file.result = response.data;
+          // });
+        }, function(response) {
+          if (response.status > 0)
+            $scope.errorMsg = response.status + ': ' + response.data;
+        });
+
+        file.upload.progress(function(evt) {
+          // Math.min is to fix IE which reports 200% sometimes
+          file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        });
+
+        file.upload.xhr(function(xhr) {
+          // xhr.upload.addEventListener('abort', function(){console.log('abort complete')}, false);
+        });
+        $scope.file = file;
+}
     });
