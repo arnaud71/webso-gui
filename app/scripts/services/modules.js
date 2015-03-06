2b47525934ff789feaf4dc0ba1e600c8079bbe4b
Clément MILLET <clement.millet@hotmail.fr>
2015-03-06 17:44:34 +0100

    Add color view for importance

--- app/scripts/services/modules.js
+++ app/scripts/services/modules.js
@@ -14,3 +14,23 @@ angular.module('mySharedService', [])
       }
     };
   });
+
+angular.module('filters', []).
+  filter('importanceColor', function() {
+    return function(num) {
+      // return String(text).replace(/<[^>]+>/gm, '');
+      var $color = 'white';
+      switch(num){
+        case 1: $color = 'green';
+        break;
+        case 2: $color = 'yellow';
+        break;
+        case 3: $color = 'orange';
+        break;
+        case 4: $color = 'red';
+        break;
+      }
+      return $color;
+    }
+  }
+);
